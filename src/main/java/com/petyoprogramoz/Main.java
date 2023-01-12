d07edcf41c985c1413875c92af73ef0bb30d9227
Árpás Péter <arpaspeter0@gmail.com>
2023-01-12 20:24:17 +0100

    Legtöbbkönyves íróig feladatok

--- src/main/java/com/petyoprogramoz/Main.java
+++ src/main/java/com/petyoprogramoz/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Statisztika stat = new Statisztika();
         stat.readInBooks();
-        System.out.println(stat.konyvek.get(0).getAuthor());
+        System.out.println(stat.konyvek.get(1).getAuthor());
+        stat.otszaznalhosszabb();
+        stat.regikonyv();
+        stat.leghosszabbkonyvadatai();
+        stat.legtobbkonyvelrendelkezo();
     }
 }
--- src/main/java/com/petyoprogramoz/Statisztika.java
+++ src/main/java/com/petyoprogramoz/Statisztika.java
@@ -1,8 +1,8 @@
 package com.petyoprogramoz;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Statisztika {
 
@@ -33,6 +33,55 @@ public class Statisztika {
         }
         System.out.println("yo mr white");
         return true;
+    }
+
+    public void otszaznalhosszabb(){
+
+        int otszaznalnagyobb = 0;
+
+        for (int i = 0; i < konyvek.size(); i++) {
+            if (konyvek.get(i).getPage_count() > 500){
+                otszaznalnagyobb++;
+            }
+        }
+        System.out.println("Ötszáznál oldalnál hosszabb könyvek száma : " + otszaznalnagyobb);
+    }
+    public boolean regikonyv(){
+        for (int i = 0; i < konyvek.size(); i++) {
+            if (konyvek.get(i).getPublish_year() < 1950){
+                System.out.println("Van 1950-nél régebbi könyv");
+                return true;
+            }
+
+        }
+        return false;
+    }
+    
+    public void leghosszabbkonyvadatai(){
+
+        int leghosszabbid = 0;
+
+        for (int i = 0; i < konyvek.size(); i++) {
+            if (konyvek.get(i).getPage_count() > konyvek.get(leghosszabbid).getPage_count()) {
+                leghosszabbid = i;
+            }
+        }
+        System.out.println("Szerző : " + konyvek.get(leghosszabbid).getAuthor());
+        System.out.println("Cím : " + konyvek.get(leghosszabbid).getTitle());
+        System.out.println("Kiadás éve : " + konyvek.get(leghosszabbid).getPublish_year());
+        System.out.println("Oldalak Száma : " + konyvek.get(leghosszabbid).getPage_count());
+    }
+
+    public void legtobbkonyvelrendelkezo(){
+
+        List<String> irokmegjelenesszam = new ArrayList();
+        for (int i = 0; i < konyvek.size(); i++) {
+            irokmegjelenesszam.add(konyvek.get(i).getAuthor());
+        }
+        String legtobbkonyvesiro = irokmegjelenesszam.stream().collect(Collectors.groupingBy(w -> w , Collectors.counting())).entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).get().getKey();
+
+        System.out.println("A legtöbb könyvvel rendelkező szerző : " + legtobbkonyvesiro);
+
     }
 
 }
